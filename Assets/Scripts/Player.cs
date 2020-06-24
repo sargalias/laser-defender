e5504c92104e9354f044f226e134786354f9613d
Spyros Argalias <sargalias@gmail.com>
2020-06-24 08:17:28 +0100

    feat(scale world + add basic player movement)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private float playerSpeed = 100.0f;
+
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement() {
+        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 move = moveDirection * Time.deltaTime * playerSpeed;
+        transform.Translate(move);
+    }
+}
