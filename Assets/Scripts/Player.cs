a2fa8df920dae313ffeffec94bfb414fbe4ab917
Spyros Argalias <sargalias@gmail.com>
2020-06-24 10:35:09 +0100

    feat(scripts/Player): Clamp player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    [SerializeField]
     private float playerSpeed = 100.0f;
 
+    [SerializeField]
+    private float paddingPercent = 0.05f;
+
+    private Camera gameCamera;
+    private Vector3 minPosition;
+    private Vector3 maxPosition;
+
     void Start() {
+        gameCamera = Camera.main;
+        minPosition = gameCamera.ViewportToWorldPoint(new Vector3(paddingPercent, paddingPercent, 0));
+        maxPosition = gameCamera.ViewportToWorldPoint(new Vector3(1 - paddingPercent, 1 - paddingPercent, 0));
     }
 
-    // Update is called once per frame
     void Update() {
         HandlePlayerMovement();
     }
 
     private void HandlePlayerMovement() {
-        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 newPosition = CalculateMovePosition();
+        Move(newPosition);
+    }
+
+    private Vector2 CalculateMovePosition() {
+        Vector2 moveDirection = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
         Vector2 move = moveDirection * Time.deltaTime * playerSpeed;
-        transform.Translate(move);
+
+        Vector2 unclampedPosition = (Vector2)transform.position + move;
+        Vector2 clampedPosition = new Vector2(
+            Mathf.Clamp(unclampedPosition.x, minPosition.x, maxPosition.x),
+            Mathf.Clamp(unclampedPosition.y, minPosition.y, maxPosition.y)
+        );
+        return clampedPosition;
+    }
+
+    private void Move(Vector2 position) {
+        transform.position = position;
     }
 }
