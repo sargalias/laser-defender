4079aee414b2cf8427fa271dcf0f7a71e736a15b
Spyros Argalias <sargalias@gmail.com>
2020-06-25 10:51:07 +0100

    feat(playerBullet): Make playerBullet move

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+    [SerializeField]
+    float speed = 100.0f;
+
+    float maxY;
+
+    void Start() {
+        Camera camera = Camera.main;
+        maxY = camera.ViewportToWorldPoint(Vector3.one).y;
+    }
+
+    void Update() {
+        Move();
+    }
+
+    private void Move() {
+        Vector2 move = Vector2.up * Time.deltaTime * speed;
+        transform.Translate(move);
+        if (transform.position.y > maxY) {
+            Destroy(gameObject);
+        }
+    }
+}
