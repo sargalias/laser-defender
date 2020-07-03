4a8a9ff5a46d8d66e3c788244b175c132e7493fa
Spyros Argalias <sargalias@gmail.com>
2020-07-03 21:21:37 +0100

    feat(spawn enemy wave): Add spawn enemy wave code

---
+++ Assets/Scripts/EnemyWave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWave : MonoBehaviour {
+    [SerializeField] GameObject enemy = null;
+    [SerializeField] float delayBetweenSpawns = 0;
+    [SerializeField] int numEnemies = 0;
+
+    private IEnumerator SpawnWave() {
+        for (int i = 0; i < numEnemies; i++) {
+            Instantiate(enemy);
+            yield return new WaitForSeconds(delayBetweenSpawns);
+        }
+        Destroy(gameObject);
+    }
+
+    private void Start() {
+        StartCoroutine(SpawnWave());
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    [SerializeField] GameObject enemy = null;
+
+    private void Start() {
+        StartCoroutine(SpawnWave());
+    }
+
+    IEnumerator SpawnWave() {
+        while (true) {
+            Instantiate(enemy);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
